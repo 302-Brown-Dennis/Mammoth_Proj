7a061078afb0a7ae8248e39c8992287917179b78
302-Brown-Dennis <87204426+302-Brown-Dennis@users.noreply.github.com>
2023-11-02 22:54:54 -0700

    InIt push

---
+++ Source/Mammoth/MammothGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MammothGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAMMOTH_API AMammothGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
