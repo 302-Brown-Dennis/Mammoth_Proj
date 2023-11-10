27d786dfa2e47a31c46deb4d93507ffaecfbba11
Cristian Ruelas <77937764+1101-Ruelas-Cristian@users.noreply.github.com>
2023-11-09 18:47:00 -0800

    Item pickup, inventory UI, etc.
    Inventory system.

---
+++ Source/Mammoth/IStruct.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "IStruct.generated.h"
+
+USTRUCT(BlueprintType)
+struct FItemStruct : public FTableRowBase
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+    int32 ItemID;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+    FName ItemName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+    FText ItemDescription;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+    UTexture2D* ItemTB;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+    int32 Stack;
+};
