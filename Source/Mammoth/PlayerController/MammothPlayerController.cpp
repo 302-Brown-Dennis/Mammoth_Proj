06ebbd7f1afdcae040f831f22efb0a2eac14921a
302-Brown-Dennis <87204426+302-Brown-Dennis@users.noreply.github.com>
2024-04-09 17:12:26 -0700

    Refactored some c++ code, more animation work, added a new enemy

--- Source/Mammoth/PlayerController/MammothPlayerController.cpp
+++ Source/Mammoth/PlayerController/MammothPlayerController.cpp
@@ -8,13 +8,36 @@
 #include "Components/ProgressBar.h"
 #include "Components/TextBlock.h"
 #include "Components/Image.h"
-
+#include "Net/UnrealNetwork.h"
+#include "Mammoth/GameModes/LevelsGameMode.h"
+#include "Mammoth/PlayerCharacter_cpp.h"
 void AMammothPlayerController::BeginPlay() {
 	Super::BeginPlay();
 
 	MammothHUD = Cast<AMammothHUD>(GetHUD());
 }
+void AMammothPlayerController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+	APlayerCharacter_cpp* PlayerCharacter = Cast<APlayerCharacter_cpp>(InPawn);
+	if (PlayerCharacter)
+	{
+		SetHUDHealth(PlayerCharacter->GetHealth(), PlayerCharacter->GetMaxHealth());
+		SetHUDStamina(PlayerCharacter->GetStamina(), PlayerCharacter->GetMaxStamina());
+	}
+}
+void AMammothPlayerController::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
+	DOREPLIFETIME(AMammothPlayerController, MatchState);
+	
+}
+void AMammothPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	//PollInit();
+}
 void AMammothPlayerController::SetHUDHealth(float Health, float MaxHealth) {
 	MammothHUD = MammothHUD == nullptr ? Cast<AMammothHUD>(GetHUD()) : MammothHUD;
 
@@ -22,12 +45,55 @@ void AMammothPlayerController::SetHUDHealth(float Health, float MaxHealth) {
 		MammothHUD->CharacterOverlay && 
 		MammothHUD->CharacterOverlay->HealthBar && 
 		MammothHUD->CharacterOverlay->HealthText;
+	
 
 	if (mHUDValid) {
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Yellow, FString(TEXT("HUD valid! setting health")));
+		}
 		const float HealthPercent = Health / MaxHealth;
 		MammothHUD->CharacterOverlay->HealthBar->SetPercent(HealthPercent);
 		FString HealthText = FString::Printf(TEXT("%d/%d"), FMath::CeilToInt(Health), FMath::CeilToInt(MaxHealth));
 		MammothHUD->CharacterOverlay->HealthText->SetText(FText::FromString(HealthText));
+	}
+	else
+	{
+		bInitHealth = true;
+		HUDHealth = Health;
+		HUDMaxHealth = MaxHealth;
+		if (MammothHUD == nullptr)
+		{
+			if (GEngine)
+			{
+				GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Yellow, FString(TEXT("HUD NOT VALID!")));
+			}
+			return;
+		}
+		if (MammothHUD->CharacterOverlay == nullptr)
+		{
+			/*if (GEngine)
+			{
+				GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Yellow, FString(TEXT("HUD -> CHARACTER OVERLAY NOT VALID!")));
+			}*/
+			return;
+		}
+		if (MammothHUD->CharacterOverlay->HealthBar == nullptr)
+		{
+			if (GEngine)
+			{
+				GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Yellow, FString(TEXT("HUD -> CHARACTER OVERLAY -> HEALTHBAR NOT VALID!")));
+			}
+			return;
+		}
+		if (MammothHUD->CharacterOverlay->HealthText == nullptr)
+		{
+			if (GEngine)
+			{
+				GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Yellow, FString(TEXT("HUD -> CHARACTER OVERLAY -> HEALT TEXT NOT VALID!")));
+			}
+			return;
+		}
 	}
 }
 
@@ -41,6 +107,12 @@ void AMammothPlayerController::SetHUDStamina(float Stamina, float MaxStamina) {
 	if (mHUDValid) {
 		const float StaminaPercent = Stamina / MaxStamina;
 		MammothHUD->CharacterOverlay->StaminaBar->SetPercent(StaminaPercent);
+	}
+	else
+	{
+		bInitStamina = true;
+		HUDStamina = Stamina;
+		HUDMaxStamina = MaxStamina;
 	}
 }
 
@@ -56,5 +128,60 @@ void AMammothPlayerController::SetHUDAmmo(int32 Ammo)
 		FString AmmoText = FString::Printf(TEXT("%d"), FMath::CeilToInt(Ammo));
 		MammothHUD->CharacterOverlay->AmmoText->SetText(FText::FromString(AmmoText));
 	}
+	else
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Yellow, FString(TEXT("HUD not valid saving ammo!")));
+		}
+		bInitAmmo = true;
+		HUDAmmo = Ammo;
+	}
 
+}
+void AMammothPlayerController::PollInit()
+{
+	if (CharacterOverlay == nullptr)
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Yellow, FString(TEXT("Character overlay is null!")));
+		}
+		if (MammothHUD && MammothHUD->CharacterOverlay) 
+		{
+			CharacterOverlay = MammothHUD->CharacterOverlay;
+			if (CharacterOverlay)
+			{
+				if (bInitHealth) SetHUDHealth(HUDHealth, HUDMaxHealth);
+				
+
+				
+				if (bInitStamina) SetHUDStamina(HUDStamina, HUDMaxStamina);
+				if (bInitAmmo) SetHUDAmmo(HUDAmmo);
+			}
+		}
+	}
+}
+void AMammothPlayerController::OnMatchStateSet(FName State)
+{
+	MatchState = State;
+	if (MatchState == MatchState::InProgress)
+	{
+		MammothHUD = MammothHUD == nullptr ? Cast<AMammothHUD>(GetHUD()) : MammothHUD;
+		if (MammothHUD)
+		{
+			MammothHUD->AddCharacterOverlay();
+		}
+	}
+}
+void AMammothPlayerController::OnRep_MatchState()
+{
+	if (MatchState == MatchState::InProgress)
+	{
+		MammothHUD = MammothHUD == nullptr ? Cast<AMammothHUD>(GetHUD()) : MammothHUD;
+		if (MammothHUD)
+		{
+			MammothHUD->AddCharacterOverlay();
+		}
+	}
 }
